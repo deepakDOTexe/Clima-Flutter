64da740e658eac57c4b0e57cdfc8f8b4dc4f036b
deepakDOTexe <iit2018116@iiita.ac.in>
2020-06-03 11:49:54 +0530

    add Location Class

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,11 +1,28 @@
 import 'package:flutter/material.dart';
 
+import '../services/location.dart';
+import '../services/location.dart';
+
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+
+  void getLocation() async{
+    Location locationProvider = Location();
+    await locationProvider.getCurrentLocation();
+    print(locationProvider.latitude);
+    print(locationProvider.longitude);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,6 +30,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
         child: RaisedButton(
           onPressed: () {
             //Get the current location
+//            getLocation();
           },
           child: Text('Get Location'),
         ),
